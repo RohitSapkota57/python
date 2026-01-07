c12126f550c95587dc858d3bbfe641cb0fc82aee
RohitSapkota57 <rohitsapkota57@gmail.com>
2026-01-07 22:35:19 +0545

    Add files via upload

---
+++ set.py
@@ -0,0 +1,24 @@
+words={"shoot":"haan","hello":"namastay","wood":"kaat"}
+word=input("enter word that u want to translate ")
+print(words[word])
+
+
+s=set()
+n=input("enter number")
+s.add(int(n))
+n=input("enter number")
+s.add(int(n))
+n=input("enter number")
+s.add(int(n))
+n=input("enter number")
+s.add(int(n))
+n=input("enter number")
+s.add(int(n))
+n=input("enter number")
+s.add(int(n))
+n=input("enter number")
+s.add(int(n))
+n=input("enter number")
+s.add(int(n))
+
+print(s)
