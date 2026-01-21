25c955008ef4a0a978ff5ec74ae57d2441082d20
RohitSapkota57 <rohitsapkota57@gmail.com>
2026-01-21 18:06:30 +0545

    oops.py

--- oops.py
+++ oops.py
@@ -1,4 +1,13 @@
 class student:
     name="hari"
 a=student()
-print(a.name)    
+
+print(a.name)
+
+#by using __init__
+class Subject:
+    def __init__(self, chapter, hour):
+        self.chapter = chapter
+        self.hour = hour
+english = Subject(2,8)
+print(english.chapter, english.hour)
