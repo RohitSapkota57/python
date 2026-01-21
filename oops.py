f30befa5505d2af2fa7ec2eca22f9c35f453fad4
RohitSapkota57 <rohitsapkota57@gmail.com>
2026-01-21 19:37:56 +0545

    oops.py

--- oops.py
+++ oops.py
@@ -5,9 +5,15 @@ a=student()
 print(a.name)
 
 #by using __init__
+#class method(constructor)& self
 class Subject:
     def __init__(self, chapter, hour):
         self.chapter = chapter
         self.hour = hour
-english = Subject(2,8)
-print(english.chapter, english.hour)
+    def full_info(self):
+        return self.chapter,self.hour
+           
+english = Subject("chapter-2","cradit hour-8")
+print(english.chapter)
+print(english.hour)
+print(english.full_info())
