e8050fd237133d0fd2570300aea5a5ee3be9e62f
RohitSapkota57 <rohitsapkota57@gmail.com>
2026-01-26 16:29:10 +0545

    oops.py

--- oops.py
+++ oops.py
@@ -17,3 +17,19 @@ english = Subject("chapter-2","cradit hour-8")
 print(english.chapter)
 print(english.hour)
 print(english.full_info())
+
+#Question 1: Basic Inheritance
+#Create a class Animal with a method sound() that prints "Animal makes a sound".
+#Create another class Dog that inherits from Animal and has a method bark() that prints "Dog barks".
+#Create an object of Dog and call both methods.
+
+class animal :
+    def sound(self):
+        print("animal make sound")
+class dog(animal):
+    def bark (self):
+        print("dog bark")
+
+d=dog()
+d.sound()
+d.bark()
